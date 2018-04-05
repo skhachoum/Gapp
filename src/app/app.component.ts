1a654f713a88e58c045c2f1a47917967049e0bed
pc-soukaina\SOUKAINA <soukainahachoum@gmail.com>
2018-04-05 17:27:42 +0100

    - galery design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,20 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
+import { GalleryPage } from "../pages/gallery/gallery";
+import { PlacesPage } from "../pages/places/places";
+import { MeteoPage } from "../pages/meteo/meteo";
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
   rootPage:any = HomePage;
+  menus = [
+    {title:'Home', component:HomePage, icon: 'home'},
+    {title:'Galerie', component:GalleryPage, icon: 'image'},
+    {title:'Adresse', component:PlacesPage, icon: 'globe'},
+    {title:'Météo', component:MeteoPage, icon: 'bolt'},
+  ]
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
@@ -17,6 +26,10 @@ export class MyApp {
       statusBar.styleDefault();
       splashScreen.hide();
     });
+  }
+
+  onPage(page: any){
+    this.rootPage= page.component;
   }
 }
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,17 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { GalleryPage } from "../pages/gallery/gallery";
+import { MeteoPage } from "../pages/meteo/meteo";
+import { PlacesPage } from "../pages/places/places";
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    GalleryPage,
+    MeteoPage,
+    PlacesPage
   ],
   imports: [
     BrowserModule,
@@ -19,7 +25,10 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    GalleryPage,
+    MeteoPage,
+    PlacesPage
   ],
   providers: [
     StatusBar,
