202d2fe62d1bf9bde6ffc49266c2bbd075407e91
pc-soukaina\SOUKAINA <soukainahachoum@gmail.com>
2018-07-31 16:23:12 +0100

    - public

--- src/app/app.customConfig.ts
+++ src/app/app.customConfig.ts
@@ -1,9 +1,9 @@
 export class AppCustomConfig {
    public static PIXABAY_API_ENDPOINT='https://pixabay.com/api/?key=';
-   public static PIXABAY_API_KEY='8601718-dc8c79692a993439da85874d6';
+   public static PIXABAY_API_KEY='YOUR_KEY';
    public static PIXABAY_API_CONFIG='&lang=fr&order=popular'
    
    public static WEATHER_API_ENDPOINT='https://api.openweathermap.org/data/2.5/';
-   public static WEATHER_API_KEY='?appid=88a5d6c7f4bd32ee0151aeeb3811091d';
+   public static WEATHER_API_KEY='?appid=YOUR_KEY';
    public static WEATHER_API_CONFIG='&lang=fr&units=metric'
 }
